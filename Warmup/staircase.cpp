be7d71b2f85f4876e6c78ca3584ce5762bcecec5
umutbalkan <umutbalkan@icloud.com>
2016-10-06 02:03:40 +0300

    new challenge

---
+++ Warmup/staircase.cpp
@@ -0,0 +1,24 @@
+/*
+    Staircase - HackerRank
+    link - https://www.hackerrank.com/challenges/staircase
+    @umut
+#include <iostream>
+using namespace std;
+
+
+int main(){
+    int n;
+    cin >> n;
+    
+    for(int i = 1; i <= n; i++){
+        for( int j = 1; j <= n; j++){
+            if( n-(j+i) >= 0){
+                cout << " ";
+            }
+            else
+                cout << "#";
+        }
+        printf("\n");
+    }
+    return 0;
+}
