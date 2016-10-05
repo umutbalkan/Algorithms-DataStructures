5d3c342e87fce1d2b65a135379395b2a6f3d5c94
umutbalkan <umutbalkan@icloud.com>
2016-10-06 00:27:53 +0300

    more

---
+++ Warmup/plusMinus.cpp
@@ -0,0 +1,36 @@
+/*
+  Plus Minus - HackerRank
+  @umut
+*/
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main(){
+    int n;
+    cin >> n;
+    int pos = 0;
+    int neg = 0;
+    int zero = 0;
+    vector<int> arr(n);
+    for(int i = 0; i < n; i++){
+       cin >> arr[i];
+        
+       if( arr[i] > 0)
+           pos++;
+       else if(!arr[i])
+           zero++;
+       else if ( arr[i] < 0)
+           neg++;
+    }
+    cout << 1/(n / (double)pos) << endl;
+    cout << 1/(n / (double)neg) << endl;
+    cout << 1/(n / (double)zero) << endl;
+    
+    return 0;
+}
