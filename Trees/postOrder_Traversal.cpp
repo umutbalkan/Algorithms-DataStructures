8227e31101c0571414eb06d4baa36f40fd61f385
umutbalkan <umutbalkan@icloud.com>
2016-10-14 11:48:26 +0300

    yey

---
+++ Trees/postOrder_Traversal.cpp
@@ -0,0 +1,24 @@
+/*
+	Post Order Traversal - HackerRank
+	@umut
+	link: https://www.hackerrank.com/challenges/tree-inorder-traversal
+*/
+
+// Node is defined as  
+struct node
+{
+    int data;
+    node* left;
+    node* right;
+};
+
+*/
+
+
+void postOrder(node *root) {
+    if(!root)
+        return; // if root is null, abort function.
+    postOrder(root->left);
+    postOrder(root->right);
+    cout << root->data << " ";
+}
