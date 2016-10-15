c0e53af50542e20ea98fd74a4f7e55b5d3ddd231
umutbalkan <umutbalkan@icloud.com>
2016-10-15 11:55:54 +0300

    Breadth-First Search

---
+++ Trees/levelOrder_Traversal.cpp
@@ -0,0 +1,27 @@
+/*
+	Level Order Traversal - HackerRank
+	@umut
+	link: https://www.hackerrank.com/challenges/tree-level-order-traversal
+*/
+
+#include <queue>
+// I need queue for this specific algorithm. Because Level-Order Traversal,in other words Breadth-First Search, can be easily implement by using queue data structure.
+
+
+void LevelOrder(node* root)
+{
+	if (root == NULL) return;
+    
+    queue<node*> q;
+	q.push(root);
+	while (!q.empty()){
+		node* n = q.front(); // .pop() is a void function, front returns the element
+        q.pop();
+		cout << n->data << " ";
+		if (n->left)
+			q.push(n->left);
+        if (n->right)
+			q.push(n->right);
+	}
+  
+}
